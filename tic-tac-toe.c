1301015bd2b9a4ca1d4f5dbbdf01aa9c8e75bead
BacqueV <admedovbahtiar55@gmail.com>
2024-12-16 22:31:18 +0500

    second & last commit. it is better to use getchar() instead of scanf(' %c') btw

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,6 +1,6 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <time.h>
 
 char board[3][3];
@@ -15,34 +15,51 @@ void computerMove();
 char checkWinner();
 void printWinner(char);
 
-int main()
-{
+int main() {
   char winner = ' ';
+  char response = ' ';
 
-  resetBoard();
-  while(winner == ' ' && checkFreeSpaces != 0)
-  {
+  do {
+    winner = ' ';
+    response = ' ';
+    resetBoard();
+
+    while (winner == ' ' && checkFreeSpaces() != 0) {
+      printBoard();
+
+      playerMove();
+      winner = checkWinner();
+      if (winner != ' ' || checkFreeSpaces() == 0) {
+        break;
+      }
+
+      computerMove();
+      winner = checkWinner();
+      if (winner != ' ' || checkFreeSpaces() == 0) {
+        break;
+      }
+    }
     printBoard();
+    printWinner(winner);
 
-    playerMove();
-    winner = checkWinner();
-  }
+    printf("\nСыграем еще? (Y/N): ");
+    getchar();
+    scanf("%c", &response);
+    response = toupper(response);
+  } while (response == 'Y');
+
+  printf("Спасибо за игру!");
   return 0;
 }
 
-
-void resetBoard()
-{
-  for(int i = 0; i < 3; i++)
-  {
-    for(int j = 0; j < 3; j++)
-    {
+void resetBoard() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
       board[i][j] = ' ';
     }
   }
 }
-void printBoard()
-{
+void printBoard() {
   printf(" %c | %c | %c", board[0][0], board[0][1], board[0][2]);
   printf("\n---|---|---\n");
   printf(" %c | %c | %c", board[1][0], board[1][1], board[1][2]);
@@ -50,55 +67,79 @@ void printBoard()
   printf(" %c | %c | %c", board[2][0], board[2][1], board[2][2]);
   printf("\n");
 }
-int checkFreeSpaces()
-{
+int checkFreeSpaces() {
   int freeSpaces = 9;
 
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      if(board[i][j] != ' ')
-      {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (board[i][j] != ' ') {
         freeSpaces--;
       }
     }
   }
   return freeSpaces;
 }
-void playerMove()
-{
+void playerMove() {
   int x;
   int y;
 
   do {
-  printf("Выберите строку #(1-3): ");
-  scanf("%d", &x);
-  x--;
-  printf("Выберите колонну #(1-3): ");
-  scanf("%d", &y);
-  y--;
+    printf("Выберите строку #(1-3): ");
+    scanf("%d", &x);
+    x--;
+    printf("Выберите колонну #(1-3): ");
+    scanf("%d", &y);
+    y--;
 
-  if(board[x][y] != ' ')
-  {
-    printf("Поле занятно!\n");
-  }
-  else
-  {
-    board[x][y] = PLAYER;
-    break;
-  }
+    if (board[x][y] != ' ') {
+      printf("Поле занятно!\n");
+    } else {
+      board[x][y] = PLAYER;
+      break;
+    }
   } while (board[x][y] != ' ');
 }
-void computerMove()
-{
+void computerMove() {
+  // creates a seed based on current time
+  srand(time(0));
+  int x;
+  int y;
 
+  do {
+    x = rand() % 3;
+    y = rand() % 3;
+  } while (board[x][y] != ' ');
+
+  board[x][y] = COMPUTER;
 }
-char checkWinner()
-{
-
+char checkWinner() {
+  // check rows
+  for (int i = 0; i < 3; i++) {
+    if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+      return board[i][0];
+    }
+  }
+  // check columns
+  for (int i = 0; i < 3; i++) {
+    if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+      return board[0][i];
+    }
+  }
+  // check diagonals
+  if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+    return board[0][0];
+  }
+  if (board[0][2] == board[1][1] && board[0][0] == board[2][0]) {
+    return board[0][2];
+  }
+  return ' ';
 }
-void printWinner(char winner)
-{
-
+void printWinner(char winner) {
+  if (winner == PLAYER) {
+    printf("Игрок победил!");
+  } else if (winner == COMPUTER) {
+    printf("Победил компьютер!");
+  } else {
+    printf("Ничья!");
+  }
 }
